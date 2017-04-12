182b185fe579519aa6a0aabb2f880bcb4f6b675a
buison <buison@users.noreply.github.com>
2017-04-12 21:37:25 +0700

    Add files via upload

---
+++ star-rating_locale_vi.js
@@ -0,0 +1,34 @@
+/*!
+ * Star Rating Ukrainian Translations
+ *
+ * This file must be loaded after 'star-rating.js'. Patterns in braces '{}', or
+ * any HTML markup tags in the messages must not be converted or translated.
+ *
+ * @see http://github.com/kartik-v/bootstrap-star-rating
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ *
+ * NOTE: this file must be saved in UTF-8 encoding.
+ *
+ * Ukrainian translate by https://github.com/wowkin2.
+ *
+ */
+(function ($) {
+    "use strict";
+    $.fn.ratingLocales.vi = {
+        defaultCaption: '{rating} Đánh giá',
+        starCaptions: {
+            0.5: 'Dở tệ',
+            1: 'Dở',
+            1.5: 'Không hay',
+            2: 'Ko hay lắm',
+            2.5: 'Bình thường',
+            3: 'Xem được',
+            3.5: 'Có vẻ hay',
+            4: 'Hay',
+            4.5: 'Rất hay',
+            5: 'Hay tuyệt'
+        },
+        clearButtonTitle: 'Xóa',
+        clearCaption: 'Chưa đánh giá'
+    };
+})(window.jQuery);
